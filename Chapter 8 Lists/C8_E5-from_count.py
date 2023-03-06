fd9f980205f2bfc04194dd0e4e334b1c5375298d
zmang2 <96471613+zmang2@users.noreply.github.com>
2023-03-06 12:04:41 +1000

    Add files via upload

--- Chapter 8 Lists/C8_E5-from_count.py
+++ Chapter 8 Lists/C8_E5-from_count.py
@@ -35,10 +35,9 @@ file = open("mbox-short.txt")
 count = 0
 
 for line in file:
-    words = line.split()
-    if len(words) == 0 or words[0]!="From":
-        continue
-    print(words[1])
-    count = count+1
+    if line.startswith("From:") and len(line)!= 0:
+        words = line.split()
+        print(words)
+        count = count+1
 
 print("There were",count,"lines in the file with From as the first word")
